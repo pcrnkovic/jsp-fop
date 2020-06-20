db0e98c458d040ef7628f59757f94019957a9dcd
Pavle Crnković <pcrnkovic@gmail.com>
2020-06-20 14:30:09 +0200

    Changed package names and version.

---
+++ src/main/java/com/pc/jspfop/tag/RepeatablePageMasterReference.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pc.jspfop.tag;
+
+import com.pc.jspfop.tag.base.EmptyTag;
+
+/**
+ *
+ * @author Pavle
+ */
+public class RepeatablePageMasterReference extends EmptyTag {
+
+    @Override
+    public String getName() {
+        return "repeatable-page-master-reference";
+    }
+    
+}
---
+++ src/main/java/com/pc/jspfop/tag/TableRow.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pc.jspfop.tag;
+
+import com.pc.jspfop.tag.base.NonEmptyTag;
+
+/**
+ *
+ * @author Pavle
+ */
+public class TableRow extends NonEmptyTag {
+
+    @Override
+    public String getName() {
+        return "table-row";
+    }
+    
+}
